c5de13304260e9c5f4d1c1bd435e3e8f18daf6ce
n.bitounis <n.bitounis@xe.gr>
2020-04-25 20:26:15 +0300

    Added notifications controller test.

---
+++ Tests/SesNotifications.App.Tests/Controllers/NotificationsControllerTests.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Moq;
+using SesNotifications.App.Controllers;
+using SesNotifications.App.Services.Interfaces;
+using Xunit;
+
+namespace SesNotifications.App.Tests.Controllers
+{
+    public class NotificationsControllerTests
+    {
+        [Fact]
+        public void Verify()
+        {
+            var mockService = new Mock<INotificationService>(MockBehavior.Strict);
+            mockService.Setup(x => x.HandleNotification(It.IsAny<string>()));
+
+            var controller = new NotificationsController(mockService.Object);
+
+            var result = controller.Post("something");
+            Assert.NotNull(result);
+
+            var response = result as OkResult;
+            Assert.NotNull(response);
+
+            mockService.Verify(x => x.HandleNotification(It.IsAny<string>()), Times.Exactly(1));
+        }
+    }
+}
