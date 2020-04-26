61dc2f648d2f2417641d7ea5bf9873478ca0adb6
n.bitounis <n.bitounis@xe.gr>
2020-04-26 14:31:09 +0300

    Find bounces page added.

---
+++ Projects/SesNotifications.App/Pages/FindBounces.cshtml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using SesNotifications.App.Services.Interfaces;
+using SesNotifications.DataAccess.Entities;
+
+namespace SesNotifications.App.Pages
+{
+    public class FindBouncesModel : PageModel
+    {
+        [BindProperty]
+        public InputModel Input { get; set; }
+
+        public IList<SesBounce> Bounces { get; set; } = new List<SesBounce>();
+
+        public class InputModel
+        {
+            [Required]
+            [DataType(DataType.Date)]
+            public DateTime Start { get; set; }
+
+            [Required]
+            [DataType(DataType.Date)]
+            public DateTime End { get; set; }
+
+            public string Email { get; set; }
+        }
+
+        private readonly ISearchService _searchService;
+
+        public FindBouncesModel(ISearchService searchService)
+        {
+            _searchService = searchService;
+        }
+
+        public IActionResult OnPost()
+        {
+            Bounces = _searchService.FindBounces(Input.Email, Input.Start, Input.End);
+            return Page();
+        }
+    }
+}
